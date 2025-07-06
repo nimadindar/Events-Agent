e97bb219c98d16fc2d28d0c86dd8a1780fda872b
nimadindar <nima.dindarsafa@gmail.com>
2025-07-06 21:39:53 +0200

    fixed encoding bug

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ import yaml
 
 def load_yaml(input_path:str) -> str:
 
-    with open(input_path, "r") as f:
+    with open(input_path, "r", encoding="utf-8") as f:
         prompt_config = yaml.safe_load(f)
 
     return prompt_config
