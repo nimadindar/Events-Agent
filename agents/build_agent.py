687c2d72be3df6a654c7e5211bc860f18a3874c7
nimadindar <nima.dindarsafa@gmail.com>
2025-07-07 10:23:23 +0200

    updated agents to create tool_calling agent

--- agents/build_agent.py
+++ agents/build_agent.py
@@ -1,8 +1,8 @@
 from typing import List
 
 from langchain_core.language_models import BaseLanguageModel
+from langchain.agents import create_tool_calling_agent
 from langchain_core.prompts import BasePromptTemplate
-from langchain.agents import create_react_agent
 from langchain_core.tools import BaseTool
 
 
@@ -19,4 +19,4 @@ class BuildAgent:
 
     def build_agent(self):
 
-        return create_react_agent(self.llm, self.tools, self.prompt)
+        return create_tool_calling_agent(self.llm, self.tools, self.prompt)
