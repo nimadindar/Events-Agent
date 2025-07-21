3b558e342dd0bc1c8ad84e4ba34b7a961dcb50d0
nimadindar <nima.dindarsafa@gmail.com>
2025-07-21 20:02:18 +0200

    added configs for scholar

--- config.py
+++ config.py
@@ -12,6 +12,11 @@ class AgentConfig:
 
     TavilyMaxResults = 5                            # maximum number of retrieved search results from tavily search tool !!! HINT: Lager number might overload LLM's contex window !!! 
     BlogMinUsefulness = 70                          # minimum usefulness score for saving the blog posts
+
+    # ScholarPages = ["2UgplA4AAAAJ","WoqSEpYAAAAJ", "e27EmFsAAAAJ", "Wnxq0mgAAAAJ"]  # google scholar pages of ["Yanan Xin", "Sebastian J. Vollmer", "Gerrit Grossmann", "Seth Flaxman"]
+    ScholarPages = ["Wnxq0mgAAAAJ"]                 # google scholar page of ["Seth Flaxman"]
+    ScholarMaxResults = 1                           # maximum number of retrieved google scholar results !!! HINT: Lager number might overload LLM's contex window !!!
+    ScholarMinUsefulness = 50                       # minimum usefulness score for saving google scholar papers
     
     PromptVersion = 3                               # system prompt version
     
@@ -22,7 +27,8 @@ class AgentConfig:
     
     InvokeInput = "Search for the related papers and blog posts and save an organized json file, and finally post a paper or blog post with the highest score to X."
 
-    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")    
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")   
+    SERP_API_KEY = os.getenv("SERP_API_KEY") 
     X_API_KEY = os.getenv("X_API_KEY")
     X_API_KEY_SECRET = os.getenv("X_API_KEY_SECRET")
     X_ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
