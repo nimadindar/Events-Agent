fe56800626371308f8c60bbc651f4c747890708c
nimadindar <nima.dindarsafa@gmail.com>
2025-07-24 11:41:22 +0200

    updated the config file

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ class AgentConfig:
     Field = "Spatio Temporal Point Process"         # the field that agent conducts research
 
     ArxivMaxResults = 5                             # maximum number of retreived papers from arxiv tool !!! HINT: Lager number might overload LLM's contex window !!! 
-    ArxivMinUsefulness = 70                         # minimum score required to save an Arxiv paper
+    ArxivMinUsefulness = 75                         # minimum score required to save an Arxiv paper
 
     TavilyMaxResults = 5                            # maximum number of retrieved search results from tavily search tool !!! HINT: Lager number might overload LLM's contex window !!! 
-    BlogMinUsefulness = 70                          # minimum usefulness score for saving the blog posts
+    BlogMinUsefulness = 75                          # minimum usefulness score for saving the blog posts
 
     # ScholarPages = ["2UgplA4AAAAJ","WoqSEpYAAAAJ", "e27EmFsAAAAJ", "Wnxq0mgAAAAJ"]  # google scholar pages of ["Yanan Xin", "Sebastian J. Vollmer", "Gerrit Grossmann", "Seth Flaxman"]
     ScholarPages = ["Wnxq0mgAAAAJ"]                 # google scholar page of ["Seth Flaxman"]
-    ScholarMaxResults = 1                           # maximum number of retrieved google scholar results !!! HINT: Lager number might overload LLM's contex window !!!
+    ScholarMaxResults = 2                           # maximum number of retrieved google scholar results !!! HINT: Lager number might overload LLM's contex window !!!
     ScholarMinUsefulness = 50                       # minimum usefulness score for saving google scholar papers
     
     PromptVersion = 4                               # system prompt version
