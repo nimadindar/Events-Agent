c4e2f5714b19173d072f904e0ac3b38789afe0ae
nimadindar <nima.dindarsafa@gmail.com>
2025-08-09 23:00:09 +0200

    changed node direction

---
+++ multi_agent/ResearchTeam/arxiv_node.py
@@ -0,0 +1,43 @@
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
+from langgraph.types import Command
+
+from ..agent_utils.utils import load_llm, State
+
+from tools.tools import ArxivTool, save_to_json
+from utils.utils import load_prompt_multi_agent
+from config import AgentConfig
+
+input_var = {
+    "field": AgentConfig.Field,
+    "arxiv_max_results": AgentConfig.ArxivMaxResults,
+    "arxiv_min_usefulness": AgentConfig.ArxivMinUsefulness
+}
+
+arxiv_system_prompt = load_prompt_multi_agent(
+    "arxiv",
+    **input_var
+)
+
+llm = load_llm(
+    llm = AgentConfig.LLM,
+    model=AgentConfig.ModelName,
+    api_key=AgentConfig.GOOGLE_API_KEY
+)
+
+arxiv_agent = create_react_agent(
+    llm,
+    tools=[ArxivTool, save_to_json],
+    prompt=arxiv_system_prompt
+)
+
+def arxiv_node(state: State) -> Command:
+    result = arxiv_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="arxiv")
+            ]
+        },
+        goto="blog"
+    )
