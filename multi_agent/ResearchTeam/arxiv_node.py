86e8610ac50ceb2fc8265cfb22dd3a46b3eddb85
Nima <nima@Nimas-MacBook-Pro.local>
2025-08-20 10:54:42 +0200

    solved dependency for config + added sample

--- multi_agent/ResearchTeam/arxiv_node.py
+++ multi_agent/ResearchTeam/arxiv_node.py
@@ -4,9 +4,9 @@ from langgraph.types import Command
 
 from ..agent_utils.utils import load_llm, State
 
-from tools.tools import ArxivTool, save_to_json
-from utils.utils import load_prompt_multi_agent
-from config import AgentConfig
+from ..tools.tools import ArxivTool, save_to_json
+from ..utils.utils import load_prompt_multi_agent
+from multi_agent.config import AgentConfig
 
 input_var = {
     "field": AgentConfig.Field,
@@ -40,4 +40,29 @@ def arxiv_node(state: State) -> Command:
             ]
         },
         goto="blog"
+        # goto = END
     )
+
+# Sample for running the agent seperately
+
+# from langchain_core.messages import HumanMessage
+# from langgraph.graph import StateGraph, START, END
+
+
+# research_builder = StateGraph(State)
+# research_builder.add_node("arxiv", arxiv_node)
+# research_builder.add_edge(START, "arxiv")
+
+# research_graph = research_builder.compile()
+
+
+# for s in research_graph.stream(
+#     {
+#         "messages": [
+#             ("user", f"Research about {AgentConfig.Field} and then give me the results.")
+#         ],
+#     },
+#     {"recursion_limit": 150},
+# ):
+#     print(s)
+#     print("---")
