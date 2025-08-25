c53c9f96bafc25da9ee8fbfdc033754ae6834153
Nima <nima@Mac.fritz.box>
2025-08-25 19:53:49 +0200

    updated blog node

--- multi_agent/ResearchTeam/blog_node.py
+++ multi_agent/ResearchTeam/blog_node.py
@@ -1,69 +1,90 @@
-from langchain_core.messages import HumanMessage
+import os
+import yaml
+from functools import partial
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 from langgraph.types import Command
 
-from ..agent_utils.utils import load_llm, State
-
 from ..tools.tools import tavily_tool, save_to_json
-from ..utils.utils import load_prompt_multi_agent
-from multi_agent.config import AgentConfig
+from ..utils.utils import State, DebugHandler
 
-input_var = {
-    "field": AgentConfig.Field,
-    "tavily_api_key": AgentConfig.TAVILY_API_KEY,
-    "tavily_max_results": AgentConfig.TavilyMaxResults,
-    "blog_min_usefulness": AgentConfig.BlogMinUsefulness
+
+# Path to system prompt
+BLOG_PROMPT_DIR = "./multi_agent/prompts/blog_node_prompt.yaml"
+
+# Prompt Config
+FIELD = "Spatio-Temporal point processing"
+BLOG_MAX_RESULTS = 1
+BLOG_MIN_USEFULNESS = 70
+
+# Tavily Config
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+
+# Model Config
+MODEL_NAME = "gemini-2.5-flash"
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+NEXT_STATE = END
+
+INPUT_VAR = {
+    "field": FIELD,
+    "tavily_api_key": TAVILY_API_KEY,
+    "tavily_max_results": BLOG_MAX_RESULTS,
+    "blog_min_usefulness": BLOG_MIN_USEFULNESS
 }
 
-blog_system_prompt = load_prompt_multi_agent(
-    "blog",
-    **input_var
-)
+with open(BLOG_PROMPT_DIR, "r", encoding="utf-8") as f:
+        prompt_config = yaml.safe_load(f)
 
-llm = load_llm(
-    llm = AgentConfig.LLM,
-    model=AgentConfig.ModelName,
-    api_key=AgentConfig.GOOGLE_API_KEY
-)
+BLOG_SYSTEM_PROMPT = prompt_config["prompt"].format(**INPUT_VAR)
 
-blog_agent = create_react_agent(
+llm = ChatGoogleGenerativeAI(
+                model=MODEL_NAME,
+                temperature=0,
+                google_api_key=GOOGLE_API_KEY
+        )
+
+arxiv_agent = create_react_agent(
     llm,
     tools=[tavily_tool, save_to_json],
-    prompt=blog_system_prompt
+    prompt=BLOG_SYSTEM_PROMPT
 )
 
-def blog_node(state: State) -> Command:
-    result = blog_agent.invoke(state)
+def blog_node(state: State, next_state) -> Command:
+    result = arxiv_agent.invoke(
+        state,
+        config={"callbacks": [DebugHandler()], "run_name": "blog_agent"})
     return Command(
         update={
             "messages": [
                 HumanMessage(content=result["messages"][-1].content, name="blog")
             ]
         },
-        goto="gscholar"
-        # goto = END
+        goto=next_state
     )
 
-# Sample for running the agent seperately
+def blog_main(next_state):
 
-# from langchain_core.messages import HumanMessage
-# from langgraph.graph import StateGraph, START, END
+    research_builder = StateGraph(State)
+    research_builder.add_node("arxiv", partial(blog_node, next_state=next_state))
+    research_builder.add_edge(START, "arxiv")
 
+    research_graph = research_builder.compile()
 
-# research_builder = StateGraph(State)
-# research_builder.add_node("blog", blog_node)
-# research_builder.add_edge(START, "blog")
+    for s in research_graph.stream(
+        {
+            "messages": [
+                ("user", f"Research about {FIELD} and then give me the results.")
+            ],
+        },
+        {"recursion_limit": 150},
+    ):
+        print(s)
+        print("---")
 
-# research_graph = research_builder.compile()
-
-
-# for s in research_graph.stream(
-#     {
-#         "messages": [
-#             ("user", f"Research about {AgentConfig.Field} and then give me the results.")
-#         ],
-#     },
-#     {"recursion_limit": 150},
-# ):
-#     print(s)
-#     print("---")
+if __name__ == "__main__":
+    blog_main(NEXT_STATE)
