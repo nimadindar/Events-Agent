24d4ad404824e644ac05e4174c402b701c9a66e6
Nima <nima@Mac.fritz.box>
2025-08-25 22:49:40 +0200

    fixed blog tool calling bug

--- multi_agent/ResearchTeam/gscholar_node.py
+++ multi_agent/ResearchTeam/gscholar_node.py
@@ -1,45 +1,92 @@
-from langchain_core.messages import HumanMessage
+import os
+import yaml
+from functools import partial
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 from langgraph.types import Command
 
-from ..agent_utils.utils import load_llm, State
-
 from ..tools.tools import get_scholar_papers, save_to_json
-from ..utils.utils import load_prompt_multi_agent
-from multi_agent.config import AgentConfig
+from ..utils.utils import State, DebugHandler
 
-input_var = {
-    "field": AgentConfig.Field, 
-    "author_ids_list": AgentConfig.ScholarPages,
-    "scholar_max_results": AgentConfig.ScholarMaxResults,
-    "scholar_min_usefulness": AgentConfig.ScholarMinUsefulness,
-    "serp_api_key": AgentConfig.SERP_API_KEY
+
+# Path to system prompt
+GSCHOLAR_PROMPT_DIR = "./multi_agent/prompts/gscholar_node_prompt.yaml"
+
+# Prompt Config
+FIELD = "Spatio Temporal Point Process, Spatio Temporal, Point Process, Contextual dataset, Survey data"
+GSCHOLAR_MAX_RESULTS = 1
+GSCHOLAR_MIN_USEFULNESS = 60
+AUTHOR_IDS = ["Wnxq0mgAAAAJ"]
+
+# Tavily Config
+SERP_API_KEY = os.getenv("SERP_API_KEY")
+
+# Model Config
+MODEL_NAME = "gemini-2.5-flash"
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+NEXT_STATE = END
+
+INPUT_VAR = {
+    "field": FIELD, 
+    "author_ids_list": AUTHOR_IDS,
+    "scholar_max_results": GSCHOLAR_MAX_RESULTS,
+    "scholar_min_usefulness": GSCHOLAR_MIN_USEFULNESS,
+    "serp_api_key": SERP_API_KEY
 }
 
-gscholar_system_prompt = load_prompt_multi_agent(
-    "gscholar",
-    **input_var
-)
+with open(GSCHOLAR_PROMPT_DIR, "r", encoding="utf-8") as f:
+        prompt_config = yaml.safe_load(f)
 
-llm = load_llm(
-    llm = AgentConfig.LLM,
-    model=AgentConfig.ModelName,
-    api_key=AgentConfig.GOOGLE_API_KEY
-)
+GSCHOLAR_SYSTEM_PROMPT = prompt_config["prompt"].format(**INPUT_VAR)
+
+llm = ChatGoogleGenerativeAI(
+                model=MODEL_NAME,
+                temperature=0,
+                google_api_key=GOOGLE_API_KEY
+        )
 
 gscholar_agent = create_react_agent(
     llm,
     tools=[get_scholar_papers, save_to_json],
-    prompt=gscholar_system_prompt
+    prompt=GSCHOLAR_SYSTEM_PROMPT
 )
 
-def gscholar_node(state: State) -> Command:
-    result = gscholar_agent.invoke(state)
+def gscholar_node(state: State, next_state) -> Command:
+    result = gscholar_agent.invoke(
+        state,
+        config={"callbacks": [DebugHandler()], "run_name": "gscholar_agent"})
     return Command(
         update={
             "messages": [
                 HumanMessage(content=result["messages"][-1].content, name="gscholar")
             ]
         },
-        goto="X"
+        goto=next_state
     )
+
+def gscholar_main(next_state):
+
+    research_builder = StateGraph(State)
+    research_builder.add_node("gscholar", partial(gscholar_node, next_state=next_state))
+    research_builder.add_edge(START, "gscholar")
+
+    research_graph = research_builder.compile()
+
+    for s in research_graph.stream(
+        {
+            "messages": [
+                ("user", f"Search for relevant papers about {FIELD} on the given google scholar pages and then save the results as a json object based on the instructions you have.")
+            ],
+        },
+        {"recursion_limit": 150},
+    ):
+        print(s)
+        print("---")
+
+if __name__ == "__main__":
+    gscholar_main(NEXT_STATE)
