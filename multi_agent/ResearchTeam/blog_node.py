22083730e5621c6e861e18815e9b1d0b4651c97f
Nima <nima@Mac.fritz.box>
2025-08-29 14:09:35 +0200

    updated for explicit file name handling

--- multi_agent/ResearchTeam/blog_node.py
+++ multi_agent/ResearchTeam/blog_node.py
@@ -9,17 +9,21 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import create_react_agent
 from langchain_core.messages import HumanMessage
 from langgraph.types import Command
+from langchain.tools import tool
 
 from ..tools.research_tools import tavily_tool, save_to_json
 from ..utils.utils import State, DebugHandler
 
+
+# Output file name: The file will be saved in ./saved/output_file_name.json
+FILE_NAME = "blog_results.json"
 
 # Path to system prompt
 BLOG_PROMPT_DIR = "./multi_agent/prompts/blog_node_prompt.yaml"
 
 # Prompt Config
 FIELD = "Spatio Temporal Point Process, Spatio Temporal, Point Process, Contextual dataset, Survey data"
-BLOG_MAX_RESULTS = 1
+BLOG_MAX_RESULTS = 5
 BLOG_MIN_USEFULNESS = 60
 
 # Tavily Config
@@ -30,10 +34,27 @@ MODEL_NAME = "gemini-2.5-flash"
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 NEXT_STATE = END
 
+DOMAINS_INCLUDED = [
+    "journals.plos.org",
+    "spatialedge.co",
+    "events2025.github.io",
+    "spacetimecausality.github.io"
+]
+
+
+# The base tools are edited for handling file names on code side for deterministic results.
+@tool("blog_save_to_json")
+def blog_save_to_json(content: dict) -> str:
+    """Save blog results to a specified path"""
+    return save_to_json.func(content=content, file_name=FILE_NAME)
+
+@tool("blog_search")
+def blog_search(query):
+     """search web using tavily tool"""
+     return tavily_tool.func(query,TAVILY_API_KEY,DOMAINS_INCLUDED,BLOG_MAX_RESULTS)
+
 INPUT_VAR = {
     "field": FIELD,
-    "tavily_api_key": TAVILY_API_KEY,
-    "tavily_max_results": BLOG_MAX_RESULTS,
     "blog_min_usefulness": BLOG_MIN_USEFULNESS
 }
 
@@ -50,7 +71,7 @@ llm = ChatGoogleGenerativeAI(
 
 blog_agent = create_react_agent(
     llm,
-    tools=[tavily_tool, save_to_json],
+    tools=[blog_search, blog_save_to_json],
     prompt=BLOG_SYSTEM_PROMPT
 )
 
